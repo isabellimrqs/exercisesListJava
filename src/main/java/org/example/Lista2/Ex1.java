71a3c9e1aa3789fa73e0f39ca729406babb21dc6
ETS-EngineeringTechnicalSchool BOT-ResearchDevelopment (CaP/ETS) <ct67ca@bosch.com>
2024-03-25 13:20:10 -0300

    finish later

---
+++ src/main/java/org/example/Lista2/Ex1.java
@@ -0,0 +1,19 @@
+package org.example.Lista2;
+import java.util.Scanner;
+public class Ex1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please, enter a age: ");
+        int age = scanner.nextInt();
+
+        if (age >= 0 && age <= 14){
+            System.out.println("You're a kiddo :D");
+        } else if  {
+
+        }
+
+
+    }
+}
